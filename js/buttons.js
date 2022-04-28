fc55c974ec7cc2b3568323f458ceb5a7b1a673bb
Nazar Kolesnikov <nazarkolesnikov57@gmail.com>
2022-04-28 17:15:24 +0300

    second commit

--- js/buttons.js
+++ js/buttons.js
@@ -1,21 +1,11 @@
-let productSize1 = document.querySelector('.product__size:nth-of-type(1)');
-let productSize2 = document.querySelector('.product__size:nth-of-type(2)');
-let productSize3 = document.querySelector('.product__size:nth-of-type(3)');
+let productSizes = document.querySelectorAll('.product__size');
 
-productSize1.addEventListener('click', function() {
-    productSize1.classList.toggle('active-1');
-    productSize2.classList.remove('active-1');
-    productSize3.classList.remove('active-1');
-});
+function activeBtn(id) {
+    for (let productSize of productSizes) {
+        if (productSize.classList.contains('active-1')) {
+            productSize.classList.remove('active-1');
+        }    
+    } 
 
-productSize2.addEventListener('click', function() {
-    productSize2.classList.toggle('active-1');
-    productSize1.classList.remove('active-1');
-    productSize3.classList.remove('active-1');
-});
-
-productSize3.addEventListener('click', function() {
-    productSize3.classList.toggle('active-1');
-    productSize1.classList.remove('active-1');
-    productSize2.classList.remove('active-1');
-})
+    document.getElementById(id).classList.toggle("active-1");
+}
